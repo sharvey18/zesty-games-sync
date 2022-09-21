1224e74ba426c1bc7b200fb0517137b9079ea0f2
Sam Harvey <s.rharveyiv@gmail.com>
2022-09-21 13:44:39 -0700

    games sync added

--- index.js
+++ index.js
@@ -2,71 +2,67 @@ const SDK = require("@zesty-io/sdk");
 const fetch = require("node-fetch");
 const util = require("util");
 require("env-yaml").config({ path: ".env.yaml" });
-// 1. Get zesty articles
-// 2. get wp articles
-// 3. check if wp article status is "publish"
-// 4. check article id
-// 5. create article in zesty for any ids that do not exist
-// 5.1 when creating an article looping through wp article tags to create array within zesty article in article_tagss field
 
-// console.log(process.env.WP_ENDPOINT);
-const articleLimit = 5;
+// console.log(process.env.GAME_ENDPOINT);
 const sdk = new SDK(process.env.ZESTY_INSTANCE_ZUID, process.env.ZESTY_TOKEN);
 
-async function getWPArticles() {
-  let res = await fetch(process.env.WP_ENDPOINT + "&count=" + articleLimit);
+async function getSunsGames() {
+  let res = await fetch(process.env.GAME_ENDPOINT);
   return await res.json();
 }
 
-async function getZestyArticles() {
+async function getZestyGames() {
   return await sdk.instance.getItems(process.env.ZESTY_MODEL);
 }
 
 // run iteration check against zesty and wp articles
 
 async function run(res) {
-  const zestyArticles = await getZestyArticles();
-  const wpArticles = await getWPArticles();
-  // console.log(wpArticles);
-  // console.log(zestyArticles);
-  wpArticles.results.items.map(async (article) => {
-    // check zesty articles for the id
-    let insertArticle = zestyArticles.data.find((zestyArticle) => {
-      console.log(zestyArticle.data.article_id);
-      console.log(article.id);
-      return zestyArticle.data.article_id === article.id;
+  const zestyGames = await getZestyGames();
+  const sunsGames = await getSunsGames();
+  let total = 0;
+  sunsGames.data.gscd.g.map(async (game) => {
+    // check zesty games for the id
+    let insertGame = zestyGames.data.find((zestyGame) => {
+      return zestyGame.data.game_id === game.gid;
     });
-    let check = typeof insertArticle === "undefined";
+    let check = typeof insertGame === "undefined";
     if (check) {
-      await createItem(article);
+      await createItem(game);
       total++;
     }
   });
-  res.send(`synced articles`);
+  res.send(`synced games`);
 }
 
-async function createItem(article) {
+async function createItem(game) {
   try {
     const newItem = await sdk.instance.createItem(process.env.ZESTY_MODEL, {
       data: {
-        article_id: article.id,
-        article_link: article.permalink,
-        article_title: article.title,
-        article_author: article?.author?.name
-          ? article.author.name
-          : "Phoenix Suns",
-        article_publish_date: article.date,
-        article_header: article.featuredImage.src,
-        article_excerpt: article.excerpt,
-        article_tagss: article?.taxonomy?.tags
-          ? Object.keys(article.taxonomy.tags).join(",")
-          : "",
+        game_id: game.gid,
+        game_code: game.gcode,
+        game_date: game.gdte,
+        home_team_time: game.htm,
+        visitor_team_time: game.vtm,
+        eastern_time: game.etm,
+        arena_name: game.an,
+        arena_city: game.ac,
+        arena_state: game.as,
+        game_status: game.st,
+        visiting_team_record: game.v.re,
+        visiting_team_abbreviation: game.v.ta,
+        visiting_team_name: game.v.tn,
+        visiting_team_city: game.v.tc,
+        home_team_record: game.h.re,
+        home_team_abbreviation: game.h.ta,
+        home_team_name: game.h.tn,
+        home_team_city: game.h.tc,
       },
       web: {
         canonicalTagMode: 1,
-        metaLinkText: article.title,
-        metaTitle: article.title,
-        metaDescription: article.excerpt.substr(0, 159),
+        metaLinkText: `${game.v.tn} vs ${game.h.tn}`,
+        metaTitle: `${game.v.tn} vs ${game.h.tn}`,
+        metaDescription: `${game.v.tn} vs ${game.h.tn}`,
       },
     });
 
@@ -76,7 +72,7 @@ async function createItem(article) {
   }
 }
 
-exports.zestyWPSync = async (req, res) => {
+exports.zestyGamesSync = async (req, res) => {
   // Set CORS headers for preflight requests
   // Allows GETs from any origin with the Content-Type header
   // and caches preflight response for 3600s
